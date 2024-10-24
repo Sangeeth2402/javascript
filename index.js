3bfb530d5eacdc10b683962ae161060d0ece18c1
SANGEETH RAJ <146965484+Sangeeth2402@users.noreply.github.com>
2024-10-24 17:40:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+// alert('warnming,,,');
+
+
+// console.log("welcome");
+
+
+// var UserNamer ="suryraj";
+// var UserAge=21;
+// var UserCity="Erode";
+
+
+/* let a =99;
+
+a=98;
+
+console.log(a); */
+
+// const a =99;
+
+// let a=98;
+
+// console.log(typeof a);
+
+
+// alert('warning...')
+//  console.log(prompt('enter Name'));
+ 
+// confirm('r u exit ?') 
+// print('')
+// close()
+// open('https://getbootstrap.com/')
+
+/* let a =10;
+
+a =a.toString()
+
+console.log(typeof a); */
+
+// parseFloat(prompt("enter your data"))
+
+/* let result = parseFloat("10.00");
+console.log(result);  */
+
+
+
+// let UserName =  "   Suryraj  ";
+
+// console.log(UserName[5]);
+// console.log(UserName.length);
+// console.log(UserName.concat(" welcome"));
+// console.log(UserName.indexOf('r'));
+// console.log(UserName.lastIndexOf('r'));
+// console.log(UserName.repeat(5));
+// console.log(UserName.toUpperCase());
+// console.log(UserName.trim());
+
+// UserName[18]=" sangeeth";
+
+// console.log(UserName);
+
+// console.log(UserName + "Hello ");
+
+// console.log(`welcome   ${UserName}     asdfghjkl;`);  
+
+
+
+// console.log(Math.PI);
+// console.log(Math.abs(90));
+// console.log(Math.ceil(9.1));
+// console.log(Math.min(90,80));
+
+// console.log(Math.max(90,79));
+// console.log(Math.pow(2,5));
+// console.log(Math.random());
+// console.log(Math.sqrt(64));
+// console.log(Math.random());
+// console.log(Math.floor(90.9));
+
+
+
+// Arthimetic    + - / % *;
+
+// Assinmnrt   = !=  =<   >=
+
+// Compare   < >  ==  === 
+
+// AND  &&  OR ||    NOT !
+
+// ++  -- 
+
+//  XOR ^
+
+// value ?  true  : false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
